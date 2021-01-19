c23e8fc9c701e47a09b3065936cc930085f7007c
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2021-01-18 22:40:02 -0300

    Componentes do grupo em k_and_s

--- src/k_and_s.vhd
+++ src/k_and_s.vhd
@@ -14,7 +14,7 @@
 -- Revision 0.01 - File Created
 --          0.02 - Moving to Vivado 2017.3
 -- Additional Comments: 
--- Trabalho de continuado para avaliação de Sistemas Digitais
+-- Para avaliação de Sistemas Digitais:
 -- Luana Santana, Michele Liese e Rodrigo Calovi
 ----------------------------------------------------------------------------------
 library IEEE;
