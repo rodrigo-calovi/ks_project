085c8e4b1712565f375fcc95cfc32d90e08ab52f
Luana <luana-santana@uergs.edu.br>
2021-01-27 20:38:38 -0300

    conferencia

--- src/data_path.vhd
+++ src/data_path.vhd
@@ -67,7 +67,7 @@ architecture rtl of data_path is
     signal bus_b : std_logic_vector (15 downto 0);
     
     -- saida do MUX (RAM ou ULA)
-    signal bus_C : std_logic_vector (15 downto 0);
+    signal bus_c : std_logic_vector (15 downto 0);
     
     -- saida da ULA para bus_c   
     signal ula_out : std_logic_vector (15 downto 0);
@@ -92,7 +92,7 @@ IR : process (clk)                                          -- processo IR
     
     begin
     
-        if (ir_enable = '1') then                           -- verifica se pode passar a instrucao ou nao, depende do ir_enable
+        if (ir_enable = '1' AND rising_edge(clk)) then                           -- verifica se pode passar a instrucao ou nao, depende do ir_enable
         instruction <= data_in;                             -- passa a instrucao data_in para instruction
         end if;
     
@@ -104,11 +104,10 @@ DECODE : process (instruction)                              -- processo DECODE
     
     begin
        
-        a_addr <= "00";                                     -- VERIFICAR
-        b_addr <= "00";                                     -- VERIFICAR
-        c_addr <= "00";                                     -- VERIFICAR
-        mem_addr <= "00000";                                -- VERIFICAR
-        decoded_instruction <= I_NOP;                       -- VERIFICAR
+        a_addr <= "00";                                     -- a_addr inicializa com 00
+        b_addr <= "00";                                     -- b_addr inicializa com 00
+        c_addr <= "00";                                     -- c_addr inicializa com 00
+        mem_addr <= "00000";                                -- mem_addr inicializa com 0000
 
         
         -----------         Other Instructions:         -----------
@@ -118,6 +117,12 @@ DECODE : process (instruction)                              -- processo DECODE
   
             
         -----------  Arithmetic and Logic Instructions:  -----------
+        
+        elsif(instruction(15 downto 8) = "10100100") then   -- OR
+            decoded_instruction <= I_OR;                    -- decoded_instruction recebe I_OR
+            b_addr <= instruction(1 downto 0);              -- b_addr recebe os bits 1 e 0
+            a_addr <= instruction(3 downto 2);              -- a_addr recebe os bits 3 e 2
+            c_addr <= instruction(5 downto 4);              -- c_addr recebe os bits 5 e 4
         
         elsif(instruction(15 downto 8) = "10100001") then   -- ADD
             decoded_instruction <= I_ADD;                   -- decoded_instruction recebe I_ADD
@@ -137,12 +142,6 @@ DECODE : process (instruction)                              -- processo DECODE
             a_addr <= instruction(3 downto 2);              -- a_addr recebe os bits 3 e 2
             c_addr <= instruction(5 downto 4);              -- c_addr recebe os bits 5 e 4
             
-        elsif(instruction(15 downto 8) = "10100100") then   -- OR
-            decoded_instruction <= I_OR;                    -- decoded_instruction recebe I_OR
-            b_addr <= instruction(1 downto 0);              -- b_addr recebe os bits 1 e 0
-            a_addr <= instruction(3 downto 2);              -- a_addr recebe os bits 3 e 2
-            c_addr <= instruction(5 downto 4);              -- c_addr recebe os bits 5 e 4
-            
          
         -----------     Data Movement Instructions:     -----------
             
@@ -154,7 +153,6 @@ DECODE : process (instruction)                              -- processo DECODE
         elsif(instruction(15 downto 8) = "10000010") then   -- STORE
             decoded_instruction <= I_STORE;                 -- decoded_instruction recebe I_STORE
             a_addr <= instruction(6 downto 5);              -- a_addr recebe os bits 6 e 5
-            b_addr <= instruction(6 downto 5);              -- b_addr recebe os bits 6 e 5
             mem_addr <= instruction(4 downto 0);            -- mem_addr recebe os bits 4, 3, 2, 1 e 0
 
          elsif(instruction(15 downto 8) = "10010001") then  -- MOVE
@@ -192,45 +190,79 @@ end process DECODE;
     
 BANCO_DE_REGISTRADORES : process (clk)                      -- processo BANCO_DE_REGISTRADORES
 
-    begin
-    
-        case  a_addr is                                     -- verifica o endereco que estao em a_addr
-            when "00" => bus_a <= reg_0;                    -- bus_a recebe o que esta em reg_0
-            when "01" => bus_a <= reg_1;                    -- bus_a recebe o que esta em reg_1
-            when "10" => bus_a <= reg_2;                    -- bus_a recebe o que esta em reg_2
-            when others => bus_a <= reg_3;                  -- bus_a recebe o que esta em reg_3
-        end case;
+    begin       
        
-        case  b_addr is                                     -- verifica o endereco que esta em b_addr
-            when "00" => bus_b <= reg_0;                    -- bus_b recebe o que esta em reg_0
-            when "01" => bus_b <= reg_1;                    -- bus_b recebe o que esta em reg_1
-            when "10" => bus_b <= reg_2;                    -- bus_b recebe o que esta em reg_2
-            when others => bus_b <= reg_3;                  -- bus_b recebe o que esta em reg_3
-        end case;
+       if (rst_n = '0' AND rising_edge(clk)) then           -- verifica se o rst_n e 0
+            reg_0 <= "0000000000000000";                    -- reg_0 recebe 0000000000000000
+            reg_1 <= "0000000000000000";                    -- reg_1 recebe 0000000000000000
+            reg_2 <= "0000000000000000";                    -- reg_2 recebe 0000000000000000
+            reg_3 <= "0000000000000000";                    -- reg_3 recebe 0000000000000000
+        end if;
+        
+                
+        if (rising_edge(clk)) then
        
-        if (write_reg_enable = '1') then                    -- verifica se o write_reg_enable esta¡ habilitado para acessar os registradores 
-       
-            case  c_addr is                                 -- verifica o endereco que esta em c_addr
-                when "00" => reg_0 <= bus_c;                -- reg_0 recebe o que esta em bus_c
-                when "01" => reg_1 <= bus_c;                -- reg_1 recebe o que esta em bus_c
-                when "10" => reg_2 <= bus_c;                -- reg_2 recebe o que esta em bus_c
-                when others => reg_3 <= bus_c;              -- reg_3 recebe o que esta em bus_c
-            end case;   
-       
-       end if;
-       
-       data_out <= bus_a;                                   -- data_out recebe o que esta em bus_a
+            case  a_addr is                                     -- verifica o endereco que estao em a_addr
+                when "00" => bus_a <= reg_0;                    -- bus_a recebe o que esta em reg_0
+                when "01" => bus_a <= reg_1;                    -- bus_a recebe o que esta em reg_1
+                when "10" => bus_a <= reg_2;                    -- bus_a recebe o que esta em reg_2
+                when others => bus_a <= reg_3;                  -- bus_a recebe o que esta em reg_3
+            end case;
+               
+            case  b_addr is                                     -- verifica o endereco que esta em b_addr
+                when "00" => bus_b <= reg_0;                    -- bus_b recebe o que esta em reg_0
+                when "01" => bus_b <= reg_1;                    -- bus_b recebe o que esta em reg_1
+                when "10" => bus_b <= reg_2;                    -- bus_b recebe o que esta em reg_2
+                when others => bus_b <= reg_3;                  -- bus_b recebe o que esta em reg_3
+            end case;
+        
+            data_out <= bus_a;                                  -- data_out recebe o que esta em bus_a
+           
+            
+            if (write_reg_enable = '1') then                    -- verifica se o write_reg_enable esta¡ habilitado para acessar os registradores 
+               
+                case  c_addr is                                 -- verifica o endereco que esta em c_addr                
+                    when "00" => reg_0 <= bus_c;                -- reg_0 recebe o que esta em bus_c
+                    when "01" => reg_1 <= bus_c;                -- reg_1 recebe o que esta em bus_c
+                    when "10" => reg_2 <= bus_c;                -- reg_2 recebe o que esta em bus_c
+                    when others => reg_3 <= bus_c;              -- reg_3 recebe o que esta em bus_c
+                end case;            
+                
+            end if;
+             
+        end if;
     
 end process BANCO_DE_REGISTRADORES;
     
 
     
-    
 ULA : process (bus_a, bus_b, operation)                     -- processo ULA 
         
     begin
         
-        if(operation = "00") then                           -- SOMA
+        if  (ula_out = "0000000000000000") then             -- verifica se a ula_out e 0000000000000000
+            zero_op_flag <= '1';                            -- zero_op_flag recebe 1            
+        
+        else        
+            zero_op_flag <= '0';                            -- zero_op_flag recebe 0  
+            
+        end if; 
+        
+        
+        if  (ula_out(15) = '1') then                        -- verifica se o bit 15 de ula_out e 1        
+            neg_op_flag <= '1';                             -- neg_op_flag recebe 1
+            
+        else        
+            neg_op_flag <= '0';                             -- neg_op_flag recebe 0
+            
+        end if;        
+        
+        
+        unsigned_overflow_flag <= '0';                      -- unsigned_overflow_flag inicializa com 0
+        signed_overflow_flag <= '0';                        -- signed_overflow_flag inicializa com 0        
+        
+        
+        if(operation = "01") then                           -- SOMA
             ula_out <= bus_a + bus_b;                       -- ula_out recebe a soma de bus_a com bus_b
             
             if (bus_a(15) = '0' AND bus_b(15) = '0') AND ula_out(15) = '1' then
@@ -245,23 +277,20 @@ ULA : process (bus_a, bus_b, operation)                     -- processo ULA
             elsif (bus_a(15) = '1' AND bus_b(15) = '0') AND (bus_b >= (NOT bus_a) - "1") then
                unsigned_overflow_flag <= '1';               -- unsigned_overflow_flag recebe 1
                 
-            elsif (bus_a(15)='1' and bus_b(15)='1') then
+            elsif (bus_a(15)= '1' and bus_b(15)= '1') then
                unsigned_overflow_flag <= '1';               -- unsigned_overflow_flag recebe 1
                
             end if;
             
                
-        elsif(operation = "01") then                        -- SUB
-            ula_out <= bus_b - bus_a;                       -- ula_out recebe a subtracao de bus_a com bus_b
+        elsif(operation = "10") then                        -- SUB
+            ula_out <= bus_a - bus_b;                       -- ula_out recebe a subtracao de bus_a com bus_b
             
             if(bus_a(15) = '0' AND bus_b(15) = '1') AND ula_out(15) = '1' then 
             signed_overflow_flag <= '1';                    -- signed_overflow_flag recebe 1
                 
             elsif (bus_a(15) = '1' AND bus_b(15) = '0') AND ula_out(15) = '0' then
             signed_overflow_flag <= '1';                    -- signed_overflow_flag recebe 1
-            
-            elsif (bus_a(15) = '0' and bus_b(15) = '0') and (bus_a >= bus_b) then
-            unsigned_overflow_flag <= '1';                  -- unsigned_overflow_flag recebe 1
             
             elsif (bus_a(15) = '1' and bus_b(15) = '1') and ((not bus_a) - "1" <= (not bus_b)-1) then
             unsigned_overflow_flag <= '1';                  -- unsigned_overflow_flag recebe 1
@@ -272,7 +301,7 @@ ULA : process (bus_a, bus_b, operation)                     -- processo ULA
             end if;
             
             
-        elsif(operation = "10") then                        -- AND    
+        elsif(operation = "11") then                        -- AND    
             ula_out <= bus_a AND bus_b;                     -- ula_out recebe a and de bus_a com bus_b
         
         
@@ -282,6 +311,28 @@ ULA : process (bus_a, bus_b, operation)                     -- processo ULA
         end if;
 
 end process ULA;
+
+
+
+FLAG_ENABLE : process (clk)
+
+    begin
+        
+        if (flags_reg_enable = '1' AND rising_edge(clk)) then   -- verifica se o flags_reg_enable esta habilitado
+            zero_op <= zero_op_flag;                            -- zero_op recebe de zero_op_flag
+            neg_op <= neg_op_flag;                              -- neg_op recebe de neg_op_flag
+            signed_overflow <= signed_overflow_flag;            -- signed_overflow recebe de signed_overflow_flag
+            unsigned_overflow <= unsigned_overflow_flag;        -- unsigned_overflow recebe de unsigned_overflow_flag
+        else
+            
+            zero_op <= '0';                                     -- zero_op recebe 0
+            neg_op <= '0';                                      -- neg_op recebe 0
+            signed_overflow <= '0';                             -- signed_overflow recebe 0
+            unsigned_overflow <= '0';                           -- unsigned_overflow recebe 0
+            
+        end if;
+    
+end process FLAG_ENABLE;
 
 
 
@@ -298,31 +349,6 @@ C_SEL_MUX : process (c_sel, data_in, ula_out)               -- processo C_SEL_MU
         end if;
         
 end process C_SEL_MUX;
-
-
-
-zero_op_flag <= '1' when ula_out = X"0000" else '0';
-neg_op_flag <= '1' when ula_out(15) = '1';
-    
-    
-    
-FLAG_ENABLE : process (clk)
-
-    begin
-    
-        zero_op <= '0';
-        neg_op <= '0';
-        signed_overflow <= '0';
-        unsigned_overflow <= '0';
-        
-        if (flags_reg_enable = '1') then                    -- verifica se o flags_reg_enable esta habilitado
-            zero_op <= zero_op_flag;                        -- zero_op recebe de zero_op_flag
-            neg_op <= neg_op_flag;                          -- neg_op recebe de neg_op_flag
-            signed_overflow <= signed_overflow_flag;        -- signed_overflow recebe de signed_overflow_flag
-            unsigned_overflow <= unsigned_overflow_flag;    -- unsigned_overflow recebe de unsigned_overflow_flag
-        end if;
-    
-end process FLAG_ENABLE;
 
 
 
@@ -345,10 +371,10 @@ PC : process (clk)                                          -- processo PC
     
     begin
         
-        if (rst_n = '0') then                               -- verifica se o rst_n voltou a ser 0
+        if (rst_n = '0' AND rising_edge(clk)) then          -- verifica se o rst_n voltou a ser 0
             program_counter <= "00000";                     -- program_counter recebe 00000 e volta ao inicio
             
-        elsif (pc_enable = '1') then                        -- verifica se o pc_enable esta habilitado
+        elsif (pc_enable = '1' AND rising_edge(clk)) then   -- verifica se o pc_enable esta habilitado
             program_counter <= pc_in;                       -- program_counter recebe pc_in
             
         end if;
